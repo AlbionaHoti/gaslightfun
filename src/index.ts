525b20f7ee4d4825dcca26f3add0ada8c15852ba
AlbionaHoti <albionahh@outlook.com>
2024-12-30 22:34:07 +0000

    add: draft actions on contest gaslight

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { character } from "./character.ts";
 import type { DirectClient } from "@ai16z/client-direct";
-
+import {  start_gaslight, reinforce_gaslight, check_conviction } from "./actions/gaslight_contest.ts";
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
@@ -41,6 +41,12 @@ export const wait = (minTime: number = 1000, maxTime: number = 3000) => {
   const waitTime =
     Math.floor(Math.random() * (maxTime - minTime + 1)) + minTime;
   return new Promise((resolve) => setTimeout(resolve, waitTime));
+};
+
+const gaslightPlugin = {
+  name: "gaslightPlugin",
+  description: "Plugin for managing a gaslighting action",
+  actions: [start_gaslight, reinforce_gaslight, check_conviction],
 };
 
 export function parseArguments(): {
@@ -221,6 +227,7 @@ export function createAgent(
       bootstrapPlugin,
       nodePlugin,
       character.settings.secrets?.WALLET_PUBLIC_KEY ? solanaPlugin : null,
+      gaslightPlugin,
     ].filter(Boolean),
     providers: [],
     actions: [],
